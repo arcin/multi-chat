8fa5678168ad1476d1f26c2b0363a75025ca4c87
Charles Arciniega <charlesarcin@gmail.com>
2014-07-19 15:03:45 -0700

    add a helper function to handle 404 errors

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ var http = require('http'),
     path = require('path'),
     mime = require('mime'),
     cache = {};
+
+function send404(response) {
+  response.writeHead(404, {'Content-Type' : 'text/plain'});
+  response.write('Error 404: Could not find what you were looking for.');
+  response.end();
+}
